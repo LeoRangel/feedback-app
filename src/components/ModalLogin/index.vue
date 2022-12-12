9a40f469213cbca82e5e045faa010a709e60c736
LeoRangel <leandrorangelm@gmail.com>
2022-12-12 20:00:01 -0300

    feat: add initial create account and login modals

---
+++ src/components/ModalLogin/index.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template lang="">
+  <div>
+    <h1>Login Modal</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="">
+
+</style>
