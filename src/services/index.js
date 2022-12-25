302e546b1ddc7e4c183581965f16c4d63ffaeb43
LeoRangel <leandrorangelm@gmail.com>
2022-12-25 19:14:48 -0300

    feat: set global loading in httpClient services

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import router from '@/router'
 import AuthService from './auth'
 import UsersService from './users'
+import { setGlobalLoading } from '@/store/global'
 
 const API_ENVS = {
   production: '',
@@ -15,6 +16,7 @@ const httpClient = axios.create({
 
 // Em toda requisição verifica se tem token e coloca na configuração do request
 httpClient.interceptors.request.use(config => {
+  setGlobalLoading(true)
   const token = window.localStorage.getItem('token')
 
   if (token) {
@@ -26,11 +28,15 @@ httpClient.interceptors.request.use(config => {
 
 // intercepta erros para disparar apenas os tipos de erros não tratados no componente
 // só vai cair no try catch do componente (handleSubmit do ModalLogin) os erros tratados abaixo
-httpClient.interceptors.response.use((response) => response, (error) => {
+httpClient.interceptors.response.use((response) => {
+  setGlobalLoading(false)
+  return response
+}, (error) => {
   const canThrowAnError = error.request?.status === 0 ||
-    error?.request?.status === 500
+  error?.request?.status === 500
 
   if (canThrowAnError) {
+    setGlobalLoading(false)
     throw new Error(error.message)
   }
 
@@ -39,6 +45,7 @@ httpClient.interceptors.response.use((response) => response, (error) => {
     router.push({ name: 'Home' })
   }
 
+  setGlobalLoading(false)
   return error
 })
 
