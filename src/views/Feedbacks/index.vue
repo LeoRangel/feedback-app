fbc4e6fb33d6e1a27e96f002c97469bd1fd3490a
LeoRangel <leandrorangelm@gmail.com>
2023-01-21 20:54:17 -0300

    feat: show filters in feedbacks page

--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -1,14 +1,62 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template lang="">
-  <div>
-    <h1 class="text-brand-main">Aqui é a Feedbacks</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </div>
+
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Feedbacks
+    </h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Details of all feedback received.
+    </p>
+  </div>
+
+  <div class="flex justify-center w-full pb-20">
+    <div class="w-4/5 max-w-6xl py-10 grid grid-cols-4 gap-2">
+      <div>
+        <h1 class="text-3xl font-black text-brand-darkgray">
+          Listing
+        </h1>
+
+        <suspense>
+          <template #default>
+            <filters
+              class="mt-8 animate__animated animate__fadeIn animate__faster"
+            />
+          </template>
+          <template #fallback>
+            <filters-loading class="mt-8" />
+          </template>
+        </suspense>
+      </div>
+
+      <div class="px-10 pt-20 col-span-3">
+        <p
+          class="text-lg text-center text-gray-800 font-regular">
+          There was an error loading the feedbacks 🥺
+        </p>
+        <p
+          class="text-lg text-center text-gray-800 font-regular">
+          No feedback received yet 🤓
+        </p>
+
+      </div>
+    </div>
   </div>
 </template>
-<script>
-export default {
 
+<script>
+import HeaderLogged from '@/components/HeaderLogged'
+import Filters from './Filters'
+import FiltersLoading from './FiltersLoading'
+
+export default {
+  components: {
+    HeaderLogged,
+    Filters,
+    FiltersLoading
+  }
 }
 </script>
-<style lang="">
-
-</style>
